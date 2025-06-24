51da266fd72444752ba5e6a0bb42a2c066cae7c5
Clint Jones <clinton.alan.jones@gmail.com>
2025-06-24 15:57:28 -0700

    Added new user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """New User registration"""
+    if request.method != "POST":
+        # Present the blank form to the user
+        form = UserCreationForm()
+    else:
+        # Recieved contents of form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            # Save the user and encrypted password to the database
+            new_user = form.save()
+            # Login the new user using the credentials provided
+            #   directly in the request
+            login(request, new_user)
+            # Forward to the home page
+            return redirect("notes:index")
+
+    # Show initial blank form or if the input is invalid
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
