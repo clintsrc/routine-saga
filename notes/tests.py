517dd521b55da8ed6408d535f4b7e6b7ca02f82d
Clint Jones <clinton.alan.jones@gmail.com>
2025-06-30 15:47:09 -0700

    Testing lint workflow triggers

--- notes/tests.py
+++ notes/tests.py
@@ -1,5 +1,11 @@
-# pylint: disable=W0611 # Unused TestCase imported
-
 from django.test import TestCase
 
-# Create your tests here.
+
+class HealthCheckTest(TestCase):
+    def test_healthz_route(self):
+        # If you have named your URL pattern, use reverse('healthz')
+        # Otherwise, you can just hardcode the path like '/healthz/'
+        response = self.client.get("/healthz/")
+        self.assertEqual(response.status_code, 200)
+        # Optionally, check response content if you return any specific data
+        self.assertJSONEqual(response.content, {"status": "ok"})
