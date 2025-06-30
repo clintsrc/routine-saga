a041c70b335631c13e85b61023fcb4e776571abd
Clint Jones <clinton.alan.jones@gmail.com>
2025-06-30 13:40:47 -0700

    Testing workflows

--- config/urls.py
+++ config/urls.py
@@ -3,16 +3,16 @@
 """
 URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
+The `urlpatterns` list maps URLs (endpoints) to views (routes)
+        Ref: https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Example methodologies:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another urls.py (this will 'build up' the full endpoint path)
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
