586cc699e08b9e722f8e98911031377d67f7f971
Clint Jones <clinton.alan.jones@gmail.com>
2025-07-15 12:15:02 -0700

    Added simple authentication for user registration api route

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,38 @@
-# pylint: disable=W0611 # No Models defined yet
+import binascii
+import os
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Model stores a unique token for each user to handle API authentication
+# NOTE: revisit later to use JWT instead ofa a simple token
+class Token(models.Model):
+    """
+    Generates an authentication token linked to a single user.
+    The token is used for API request authententication
+    """
+
+    # The token string
+    key = models.CharField(max_length=40, primary_key=True)
+    # One token per user. When a user is deleted this will also
+    # (cascade) delete their token
+    user = models.OneToOneField(
+        User, related_name="auth_token", on_delete=models.CASCADE
+    )
+    # When a user is created, automatically add a timestamp
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Override save() to automatically generate a token key if it doesn't exist
+        """
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
+
+    # Return a binary encoded token
+    def generate_key(self):
+        """
+        Generate a binary token, encode it as a hex string
+        """
+        return binascii.hexlify(os.urandom(20)).decode()
--- accounts/urls.py
+++ accounts/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     # Any additional pages available for this app...
     # User Registration
     path("register/", views.register, name="register"),
+    # REST API endpoints
+    path("api/register/", views.api_register, name="api-register"),
 ]
